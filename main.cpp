cf60084502036686f93069859e0c77022fbe268d
sakai-waipa <sakai-waipa@users.noreply.github.com>
2024-02-09 07:48:54 +0000

    Automated fixes by GitHub Actions

--- main.cpp
+++ main.cpp
@@ -10,304 +10,245 @@
 #include "b.h"
 #include <lib/main.h>
 
-//This should be 1 liner
-class EmptyClass
-{};
+// This should be 1 liner
+class EmptyClass {};
 
-struct EmptyStruct
-{}; // dummy comment
+struct EmptyStruct {}; // dummy comment
 
-class NotEmptyClass
-{
-	int a;
+class NotEmptyClass {
+  int a;
 };
 
-template <class T>
-class EmptyTemplate
-{};
+template <class T> class EmptyTemplate {};
 
-template <typename K, typename V = list<K>>
-class hash
-{};
+template <typename K, typename V = list<K>> class hash {};
 
 void emptyFunction() {}
 
 int emptyFunction2(int a) {}
 
-void notEmptyFunction()
-{
-	auto oneLineLambda = []() {};
-	oneLineLambda();
+void notEmptyFunction() {
+  auto oneLineLambda = []() {};
+  oneLineLambda();
 
-	auto oneLineLambda = [](int a) { print(a); };
-	oneLineLambda();
+  auto oneLineLambda = [](int a) { print(a); };
+  oneLineLambda();
 
-	auto longLineLambda = [](int a, int b, int casfdasfasdfhasdkjfhaskdjfhkasdhfkashdfjkhaskfjhas) {
-		print(a);
-	};
-	longLineLambda();
+  auto longLineLambda =
+      [](int a, int b, int casfdasfasdfhasdkjfhaskdjfhkasdhfkashdfjkhaskfjhas) {
+        print(a);
+      };
+  longLineLambda();
 }
 
-namespace EmptyNamespace
-{
-}
+namespace EmptyNamespace {}
 
 #if FOO
-#	if BAR
-#		include <foo>
-#	endif
+#if BAR
+#include <foo>
+#endif
 #endif
 
-#define EXAMPLE                                                                                    \
-	do                                                                                             \
-	{                                                                                              \
-		int x = aaaaa;                                                                             \
-		int b;                                                                                     \
-		int dddddddddd;                                                                            \
-	} while(0)
+#define EXAMPLE                                                                \
+  do {                                                                         \
+    int x = aaaaa;                                                             \
+    int b;                                                                     \
+    int dddddddddd;                                                            \
+  } while (0)
 
-template <typename T, int size>
-bool is_sorted(T (&array)[size])
-{
-	return std::adjacent_find(array, array + size, std::greater<T>()) == array + size;
+template <typename T, int size> bool is_sorted(T (&array)[size]) {
+  return std::adjacent_find(array, array + size, std::greater<T>()) ==
+         array + size;
 }
 
-int sampleCallback(int a, int beforeCallMe)
-{
-	{
-		{
-			{
-				{
-					{
-						{
-							{
-								{
-									{
-										{
-											{
-												{
-													{
-														{
-															{
-																{
-																	{
-																		{
-																			{
-																				{
-																					int aaa =
-																						bbbbbbbbbbbbbbb +
-																						ccccccccccccccc;
-																					return a *
-																						   beforeCallMe;
-																				}
-																			}
-																		}
-																	}
-																}
-															}
-														}
-													}
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+int sampleCallback(int a, int beforeCallMe) {
+  {
+    {
+      {
+        {
+          {
+            {
+              {
+                {
+                  {
+                    {
+                      {
+                        {
+                          {
+                            {
+                              {
+                                {
+                                  {
+                                    {
+                                      {
+                                        {
+                                          int aaa =
+                                              bbbbbbbbbbbbbbb + ccccccccccccccc;
+                                          return a * beforeCallMe;
+                                        }
+                                      }
+                                    }
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
-namespace A
-{
-namespace B
-{
-namespace C
-{
-namespace D
-{
+namespace A {
+namespace B {
+namespace C {
+namespace D {
 
-int veryLongFunction(int anotherArgument,
-					 int beforeCallMe,
-					 bool isThisOkForYouMan,
-					 std::vector<int> unknownValueForThisParameter)
-{
-	int a; // My comment a
-	int b = 2; // comment  b
-	return a * beforeCallMe;
+int veryLongFunction(int anotherArgument, int beforeCallMe,
+                     bool isThisOkForYouMan,
+                     std::vector<int> unknownValueForThisParameter) {
+  int a;     // My comment a
+  int b = 2; // comment  b
+  return a * beforeCallMe;
 }
 
-//clang should restore comment if missing and remove everything afterwords
+// clang should restore comment if missing and remove everything afterwords
 } // namespace D
 } // namespace C
 } // namespace B
 } // namespace A
 
-namespace A
-{
-namespace C
-{
+namespace A {
+namespace C {
 int veryLongFunctionAbcAASKJHGAKLSFNCJKASDNCUIABSALSKJDNKAJSHDKJASHDJHKJASDHKJAHBSD(
-	int anotherArgument,
-	int beforeCallMe,
-	bool isThisOkForYouMan,
-	std::vector<int> unknownValueForThisParameter)
-{
-	int a; // My comment a
-	int b = 2; // comment  b
-	return a * beforeCallMe;
+    int anotherArgument, int beforeCallMe, bool isThisOkForYouMan,
+    std::vector<int> unknownValueForThisParameter) {
+  int a;     // My comment a
+  int b = 2; // comment  b
+  return a * beforeCallMe;
 }
 } // namespace C
 } // namespace A
 
-int main()
-{
-	std::srand(std::time(0));
+int main() {
+  std::srand(std::time(0));
 
-	int list[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  int list[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-	do
-	{
-		std::random_shuffle(list, list + 9);
-	} while(is_sorted(list));
+  do {
+    std::random_shuffle(list, list + 9);
+  } while (is_sorted(list));
 
-	int score = 0;
+  int score = 0;
 
-	do
-	{
-		std::cout << "Current list: ";
-		std::copy(list, list + 9, std::ostream_iterator<int>(std::cout, " "));
+  do {
+    std::cout << "Current list: ";
+    std::copy(list, list + 9, std::ostream_iterator<int>(std::cout, " "));
 
-		int rev;
-		while(true)
-		{
-			std::cout << "\nDigits to reverse? ";
-			std::cin >> rev;
-			if(rev > 1 && rev < 10)
-			{
-				break;
-			}
-			std::cout << "Please enter a value between 2 and 9.";
-		}
+    int rev;
+    while (true) {
+      std::cout << "\nDigits to reverse? ";
+      std::cin >> rev;
+      if (rev > 1 && rev < 10) {
+        break;
+      }
+      std::cout << "Please enter a value between 2 and 9.";
+    }
 
-		++score;
-		std::reverse(list, list + rev);
-	} while(!is_sorted(list));
+    ++score;
+    std::reverse(list, list + rev);
+  } while (!is_sorted(list));
 
-	std::cout << "Congratulations, you sorted the list.\n"
-			  << "You needed " << score << " reversals." << std::endl;
+  std::cout << "Congratulations, you sorted the list.\n"
+            << "You needed " << score << " reversals." << std::endl;
 
-	auto dialog = PandemicDialog::create(i18n::tr("$QuitGameId"));
-	dialog->addButton(
-		i18n::tr("$OkId"), "common/green_frame.9.png", [this](PandemicDialog* dialog) {
-			std::cout << "Congratulations, you sorted the list.\n"
-					  << "You needed " << score << " reversals." << std::endl;
-			inherited::onBack();
-		});
-	dialog->addButton(i18n::tr("$CancelId"),
-					  "common/red_frame.9.png",
-					  [this](PandemicDialog* dialog) { dialog->close(); });
-	dialog->setTag(dialogTag);
-	dialog->show(this);
+  auto dialog = PandemicDialog::create(i18n::tr("$QuitGameId"));
+  dialog->addButton(i18n::tr("$OkId"), "common/green_frame.9.png",
+                    [this](PandemicDialog *dialog) {
+                      std::cout << "Congratulations, you sorted the list.\n"
+                                << "You needed " << score << " reversals."
+                                << std::endl;
+                      inherited::onBack();
+                    });
+  dialog->addButton(i18n::tr("$CancelId"), "common/red_frame.9.png",
+                    [this](PandemicDialog *dialog) { dialog->close(); });
+  dialog->setTag(dialogTag);
+  dialog->show(this);
 
-	try
-	{
-		foo();
-	}
-	catch()
-	{
-		//empty catch
-	}
+  try {
+    foo();
+  } catch () {
+    // empty catch
+  }
 
-	try
-	{
-		foo();
-	}
-	catch()
-	{}
+  try {
+    foo();
+  } catch () {
+  }
 
-	return 0;
+  return 0;
 }
 
 #define min(a, b) ((a) < (b) ? (a) : (b))
 
 int doSomething(...);
 
-int doSomethingElse(...)
-{
-	return 2;
-}
+int doSomethingElse(...) { return 2; }
 
-struct Foo : private FooBase
-{
+struct Foo : private FooBase {
 public:
-	int i;
+  int i;
 
-	virtual int action(int, char, float) = 0;
+  virtual int action(int, char, float) = 0;
 
-	virtual Foo* getSelf()
-	{
-		return this;
-	}
+  virtual Foo *getSelf() { return this; }
 
 private:
-	static int method(){};
-	list<Bar> bar;
+  static int method(){};
+  list<Bar> bar;
 };
 
-namespace fooNS
-{
-class FooClass : Foo, virtual FooBase
-{
+namespace fooNS {
+class FooClass : Foo, virtual FooBase {
 // comment start ar first column
 #define FooClass_B FooBase
 
-	typedef int (FooClass::*ACTION)(int);
+  typedef int (FooClass::*ACTION)(int);
 
 public:
-	FooClass()
-	{
-		act = &FooClass::nv_action;
-	}
+  FooClass() { act = &FooClass::nv_action; }
 
-	virtual ~FooClass() {}
+  virtual ~FooClass() {}
 
-	int nv_action(int arg)
-	{
-		return arg;
-	}
+  int nv_action(int arg) { return arg; }
 
-	virtual int action(int color, char alpha, float);
+  virtual int action(int color, char alpha, float);
 
-	virtual Foo* getSelf()
-	{
-		return Foo::getSelf();
-	}
+  virtual Foo *getSelf() { return Foo::getSelf(); }
 
-	int method()
-	{
-		return 0;
-	}
+  int method() { return 0; }
 
-	ACTION act;
+  ACTION act;
 
 private:
-	int var;
+  int var;
 };
 } // namespace fooNS
 
-int fooNS::FooClass::action(int color, char alpha, float)
-{
-	return doSomething(color);
+int fooNS::FooClass::action(int color, char alpha, float) {
+  return doSomething(color);
 }
 
-namespace A
-{
-namespace B
-{
+namespace A {
+namespace B {
 typedef void(fn)(int i, int j, int k);
 
 typedef void (*block)(int i, int j, int k);
@@ -315,167 +256,128 @@ typedef void (*block)(int i, int j, int k);
 } // namespace A
 typedef int X;
 
-int& refTest(X&& x)
-{
-	int**& p = (int**&)x;
-	int static& r = *&x;
-	return r && (r & x) ? r : x;
+int &refTest(X &&x) {
+  int **&p = (int **&)x;
+  int static &r = *&x;
+  return r && (r & x) ? r : x;
 }
 
 // todo something
-void doSomething(int y, int b, void* (*)())
-{
-	int a = 1 || 0 && 1;
-	int bb = a == !1 && a != 0;
-	int ccc = bb = a = 1 < 2 >= 3;
-	int dddd = ccc = bb = a = ~1 | 2 & 3 ^ 4;
+void doSomething(int y, int b, void *(*)()) {
+  int a = 1 || 0 && 1;
+  int bb = a == !1 && a != 0;
+  int ccc = bb = a = 1 < 2 >= 3;
+  int dddd = ccc = bb = a = ~1 | 2 & 3 ^ 4;
 
-	void* p1 = reinterpret_cast<void*>(&a);
-	void** p2 = &p1;
+  void *p1 = reinterpret_cast<void *>(&a);
+  void **p2 = &p1;
 
-	a = bb = ccc = dddd = 2;
-	dddd = ccc = bb = a = (1 + 2 + 3 + 4 + 5 + 0xFFFFFFFFF);
+  a = bb = ccc = dddd = 2;
+  dddd = ccc = bb = a = (1 + 2 + 3 + 4 + 5 + 0xFFFFFFFFF);
 
-	int i5 = ((1) + 2) - (4 * 5 / 6 % 7);
-	int i6 = -123456 << 2 >> 3 >> 12345;
-	int i7 = 2 > 3 ? 7 + 8 + 9 : 11 + 12 + 13;
-	int i8 = 2 < 3 + 7 + 8 + 9 ?: 11 + 12 + 13;
-	int ii[6], jj[] = {1, 2, 3, 0x000A, 0x000B, 0x000C};
+  int i5 = ((1) + 2) - (4 * 5 / 6 % 7);
+  int i6 = -123456 << 2 >> 3 >> 12345;
+  int i7 = 2 > 3 ? 7 + 8 + 9 : 11 + 12 + 13;
+  int i8 = 2 < 3 + 7 + 8 + 9 ?: 11 + 12 + 13;
+  int ii[6], jj[] = {1, 2, 3, 0x000A, 0x000B, 0x000C};
 
-	fooNS::FooClass object, *ptr = (fooNS::FooClass*)object.getSelf()->getSelf()->getSelf();
-	(object.*object.act)(1);
-	ptr->action(0xFF0000, 0.01, 320);
-	ptr->getSelf()->getSelf()->getSelf()->getSelf();
+  fooNS::FooClass object,
+      *ptr = (fooNS::FooClass *)object.getSelf()->getSelf()->getSelf();
+  (object.*object.act)(1);
+  ptr->action(0xFF0000, 0.01, 320);
+  ptr->getSelf()->getSelf()->getSelf()->getSelf();
 
-	doSomething(ii[1], jj[ii[2]], doSomething(123));
+  doSomething(ii[1], jj[ii[2]], doSomething(123));
 
-	if(1)
-	{
-		doSomething(1);
-	}
-	else if(2)
-	{
-		doSomething(1, 2);
-	}
-	if(1)
-	{
-		doSomething();
-	}
-	else if(2)
-	{
-		doSomething();
-	}
-	else
-	{
-		doSomething();
-	}
-	for(int i = 1, j = 2; i <= j; i++, j--)
-	{
-		doSomethingElse();
-	}
-	while(1)
-	{
-		doSomethingElse();
-	}
-	do
-	{
-		doSomethingElse();
-	} while(1);
-	switch(1)
-	{
-	case 0:
-		return;
-	case 1:
-	{
-		return;
-	}
-	}
+  if (1) {
+    doSomething(1);
+  } else if (2) {
+    doSomething(1, 2);
+  }
+  if (1) {
+    doSomething();
+  } else if (2) {
+    doSomething();
+  } else {
+    doSomething();
+  }
+  for (int i = 1, j = 2; i <= j; i++, j--) {
+    doSomethingElse();
+  }
+  while (1) {
+    doSomethingElse();
+  }
+  do {
+    doSomethingElse();
+  } while (1);
+  switch (1) {
+  case 0:
+    return;
+  case 1: {
+    return;
+  }
+  }
 
-	try
-	{
-		doSomethingElse();
-	}
-	catch(char* message)
-	{
-		return;
-	}
+  try {
+    doSomethingElse();
+  } catch (char *message) {
+    return;
+  }
 }
 
-struct fooS
-{
-	int i;
-	char j;
+struct fooS {
+  int i;
+  char j;
 } foo_t;
 
-struct
-{
-	int i;
-	char j;
+struct {
+  int i;
+  char j;
 } c_style_t;
 
-enum class NotSingleLineEnum
-{
-	a
-};
+enum class NotSingleLineEnum { a };
 
-enum fooE
-{
-	SUNDAY = 111,
-	MONDAY = 222,
-	TUESDAY = 333,
-	WEDNESDAY = TUESDAY + 1
+enum fooE {
+  SUNDAY = 111,
+  MONDAY = 222,
+  TUESDAY = 333,
+  WEDNESDAY = TUESDAY + 1
 } foo_e;
 
-template <typename T, typename M>
-inline T const& Min(T const& a, M const& b)
-{
-	return a < b ? a : b;
+template <typename T, typename M> inline T const &Min(T const &a, M const &b) {
+  return a < b ? a : b;
 }
 
-template <class T>
-struct FooT
-{
-	hash<int, list<char>> elems;
+template <class T> struct FooT {
+  hash<int, list<char>> elems;
 
-	template <int N>
-	int foo()
-	{
-		return N;
-	}
+  template <int N> int foo() { return N; }
 
-	template <>
-	int foo<2>()
-	{
-		return Min<>(1, 5);
-	}
+  template <> int foo<2>() { return Min<>(1, 5); }
 
-	list<int> mem = {1, 2, 3};
-	float vector[3];
+  list<int> mem = {1, 2, 3};
+  float vector[3];
 
-	FooT()
-		: elems{{-1, {'c', 'p', 'p'}}, {1, {'j', 'b'}}}
-		, vector{1f, 2f, 3f}
-	{
-		auto la = [=, this, &mem, elems](int i1, int i2) -> bool mutable { return i1 < i2; }(1, 2);
-	}
+  FooT() : elems{{-1, {'c', 'p', 'p'}}, {1, {'j', 'b'}}}, vector{1f, 2f, 3f} {
+    auto la = [=, this, &mem, elems](int i1, int i2) -> bool mutable {
+      return i1 < i2;
+    }(1, 2);
+  }
 
-	int a1, b1;
+  int a1, b1;
 
-	FooT(int a)
-		: a(1)
-		, b(2)
-	{
-		auto la = [=, this, &mem, elems](int i1, int i2) -> bool mutable { return i1 < i2; }(1, 2);
-	}
+  FooT(int a) : a(1), b(2) {
+    auto la = [=, this, &mem, elems](int i1, int i2) -> bool mutable {
+      return i1 < i2;
+    }(1, 2);
+  }
 
-	auto f(T t) -> decltype(t + doSomething())
-	{
-		return t + doSomething();
-	}
+  auto f(T t) -> decltype(t + doSomething()) { return t + doSomething(); }
 };
 
 // No extra space before brace initialization
 EmptyStruct emptyStruct = EmptyStruct{}; // otherwise: = EmptyStruct {};
 
-//comment
-EmptyStruct emptyStruct = EmptyStruct{}; //1 space after ';'' otherwise: ;//1 space after...
+// comment
+EmptyStruct emptyStruct =
+    EmptyStruct{}; // 1 space after ';'' otherwise: ;//1 space after...
